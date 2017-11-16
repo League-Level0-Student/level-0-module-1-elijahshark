eae29f72a201b82ba024b07b9105b9616cf42f48
elijahshark <league@WorthyMini-6.attlocal.net>
2017-11-15 16:57:02 -0800

    Unbirthday, Riddler (IP)
    thank for your work

---
+++ src/UNbirthday.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class UNbirthday {
+	public static void main(String[] args) {
+	String UNbirthday = JOptionPane.showInputDialog("what is your birthday");
+	if (UNbirthday.equals("11/15")) {
+		JOptionPane.showMessageDialog(null, " Happy Birthday ");	
+	} else {
+		JOptionPane.showMessageDialog(null, "Merry happy UNbirthday");
+	
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	}
+}
