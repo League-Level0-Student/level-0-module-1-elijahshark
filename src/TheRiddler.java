85d88d3fc7e1e4ee6aaba54449efbf4fa6846511
100297 <league@WTS7.attlocal.net>
2017-11-29 16:59:05 -0800

    Riddler

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageTypeSpecifier;
 import javax.swing.JOptionPane;
 
 //    Copyright (c) The League of Amazing Programmers 2013-2017
@@ -11,17 +12,30 @@ public class TheRiddler {
 
 		// 1. Make a variable to hold the score
 		int score = 0;
-		if(score == 5);
+		if(score == 2);
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String question = JOptionPane.showInputDialog(null,"Man. He crawls on all fours as a baby, then walks on two feet as an adult, and then walks with a cane as an old man.");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (question.equals("man")) {
+	 JOptionPane.showMessageDialog(null,"CORRECT");
+score ++ ;
+} else {
+	JOptionPane.showMessageDialog(null,"WRONG");
+	score -- ;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
+String question2 = JOptionPane.showInputDialog(null,"Paul's height is six feet, he's an assistant at a butcher's shop, and wears size 9 shoes. What does he weigh?");
 
 		// 6. Add some more riddles
-
+if (question2.equals("meat")) {
+	 JOptionPane.showMessageDialog(null,"CORRECT");
+score ++ ;
+} else {
+	JOptionPane.showMessageDialog(null,"WRONG");
+	score -- ;
+}
 		// 2. Make a pop up to show the score.
-		JOptionPane.showConfirmDialog(int, )
+JOptionPane.showMessageDialog(null,score);	
 	}
 }
 
