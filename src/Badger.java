16fb67d692fc977a0f03ce3efe7336ec226aeeb5
peter <league@WTS19.attlocal.net>
2017-12-13 16:57:29 -0800

    Badger (IP)

--- src/Badger.java
+++ src/Badger.java
@@ -1,4 +1,18 @@
+import javax.swing.JOptionPane;
 
 public class Badger {
+	public static void main(String[] args) {
 
+		for (int i = 0; i < 3; i++) {
+
+			for (int i1 = 0; i1 < 10; i1++) {
+				System.out.print("Badger, ");
+			}
+			System.out.println();
+			for (int i2 = 0; i2 < 2; i2++) {
+				System.out.print("Mushroom, ");
+			}
+		}
+
+	}
 }
